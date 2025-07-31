9afc7ebbc5c61249fa32e56774135590693da466
IgorCamposRatao <igorcamposratao@gmail.com>
2025-07-30 22:57:16 -0300

    paginas criadas

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import IdentificacaoScreen from './screens/IdentificacaoScreen';
+import AvaliacaoScreen from './screens/AvaliacaoScreen';
+import ConfirmacaoScreen from './screens/ConfirmacaoScreen';
+import AdminLoginScreen from './screens/AdminLoginScreen';
+import AdminDashboardScreen from './screens/AdminDashboardScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Identificacao" component={IdentificacaoScreen} />
+        <Stack.Screen name="Avaliacao" component={AvaliacaoScreen} />
+        <Stack.Screen name="Confirmacao" component={ConfirmacaoScreen} />
+        <Stack.Screen name="AdminLogin" component={AdminLoginScreen} />
+        <Stack.Screen name="AdminDashboard" component={AdminDashboardScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
